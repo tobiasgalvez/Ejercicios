cf8f329a45cdaa56d6848fe2d11bacc2dacc8f4b
Tobias Galvez <you@example.com>
2023-03-28 19:47:52 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio I01 - Máximo, mínimo y promedio/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+
+        /*
+         Consigna
+        Ingresar 5 números por consola, guardándolos en una variable escalar. 
+        Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.*/
+
+
+        static void Main(string[] args)
+        {
+            string numeroIngresadoString;
+            int numeroIngresadoParseado = 0;
+            bool resultadoDelParseo;
+
+            int maximoDeNumerosIngresados = 0;
+            int minimoDeNumerosIngresados = 0;
+
+            int acumuladorDeNumerosIngresados = 0;
+
+            float promedioNumerosIngresados = 0f;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numeroIngresadoString = Console.ReadLine();
+                resultadoDelParseo = int.TryParse(numeroIngresadoString, out numeroIngresadoParseado);
+
+                if (resultadoDelParseo == true)
+                {
+                    acumuladorDeNumerosIngresados += numeroIngresadoParseado;
+
+                    if (i == 0)
+                    {
+                        minimoDeNumerosIngresados = numeroIngresadoParseado;
+                        maximoDeNumerosIngresados = numeroIngresadoParseado;
+                    }
+                    else
+                    {
+                        if (maximoDeNumerosIngresados < numeroIngresadoParseado)
+                        {
+                            maximoDeNumerosIngresados = numeroIngresadoParseado;
+                        }
+                        if (minimoDeNumerosIngresados > numeroIngresadoParseado)
+                        {
+                            minimoDeNumerosIngresados = numeroIngresadoParseado;
+                        }
+                    }
+                }
+            }
+
+            promedioNumerosIngresados = (float)(acumuladorDeNumerosIngresados / 5);
+
+            Console.WriteLine($"Valor máximo = {maximoDeNumerosIngresados} --- Valor mínimo = {minimoDeNumerosIngresados} --- Promedio = {promedioNumerosIngresados} ");
+            //Console.WriteLine($"Valor mínimo = {min}");
+            //Console.WriteLine($"Promedio ={promedio}");
+
+        }
+    }
+}
---
+++ Ejercicio I02 - Error al cubo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+/*Consigna
+Ingresar un número y mostrar el cuadrado y el cubo del mismo. 
+Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: 
+"ERROR. ¡Reingresar número!".*/
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroIngresadoString;
+            int numeroIngresadoParseado;
+
+
+            Console.WriteLine("Ingrese un numero: ");
+            numeroIngresadoString = Console.ReadLine();
+
+            while ((int.TryParse(numeroIngresadoString, out numeroIngresadoParseado) != true) || numeroIngresadoParseado <= 0)
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número! :");
+                numeroIngresadoString = Console.ReadLine();
+
+            }
+
+            // Math.Pow(numero, 2);
+            Console.WriteLine($"El cuadrado del numero '{numeroIngresadoParseado}' es: {Math.Pow(numeroIngresadoParseado, 2)}");
+            //Math.Pow(numero, 3);
+            Console.WriteLine($"El cubo del numero es: {Math.Pow(numeroIngresadoParseado, 3)}");
+
+        }
+    }
+}
