94a616bf197b7e8626890b3b2bb83802f1db26a3
tobiasgalvez <tobi.pes@hotmail.com>
2023-04-04 13:16:56 -0300

    Ejercicio I03 - El ejemplo universal

---
+++ Biblioteca de Clases/Estudiante.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_de_Clases
+{
+    public class Estudiante
+    {
+        private int legajo;
+        private string nombre;
+        private string apellido;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            random = new Random();
+        }
+        public Estudiante(int legajo, string nombre, string apellido)
+        {
+            this.legajo = legajo;
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+
+        public void SetNotaPrimerParcial(int nota)
+        {
+            notaPrimerParcial = nota;
+        }
+
+        public void SetNotaSegundoParcial(int nota)
+        {
+            notaSegundoParcial = nota;
+        }
+
+        private float CalcularPromedio()
+        {
+            return (float)(notaPrimerParcial + notaSegundoParcial) / 2;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            if(notaPrimerParcial >= 4 && notaSegundoParcial >= 4)
+            {
+                return random.Next(6,10);
+            }
+            return -1;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("============================================");
+            sb.AppendLine(nombre);
+            sb.AppendLine(apellido);
+            sb.AppendLine(legajo.ToString());
+            sb.AppendLine($"Nota primer parcial:{notaPrimerParcial}");
+            sb.AppendLine($"Nota segundo parcial:{notaSegundoParcial}");
+            sb.AppendLine($"Promedio: {CalcularPromedio()}");
+            if (CalcularNotaFinal() != -1)
+            {
+                sb.AppendLine($"Nota final: {CalcularNotaFinal()}");
+            }
+            else
+            {
+                sb.AppendLine("Alumno desaprobado");
+            }
+            sb.AppendLine("=============================================");
+
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ Ejercicio I03 - El ejemplo universal/Program.cs
@@ -0,0 +1,40 @@
+﻿using Biblioteca_de_Clases;
+
+namespace Ejercicio_I03___El_ejemplo_universal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante[] estudiantes = new Estudiante[3];
+
+            estudiantes[0] = new Estudiante(1000, "Tobias", "Galvez");
+            estudiantes[1] = new Estudiante(1001, "Nicolas", "Sanchez");
+            estudiantes[2] = new Estudiante(1002, "Juan", "Perez");
+
+            estudiantes[0].SetNotaPrimerParcial(10);
+            estudiantes[0].SetNotaSegundoParcial(7);
+
+            estudiantes[1].SetNotaPrimerParcial(4);
+            estudiantes[1].SetNotaSegundoParcial(3);
+
+            estudiantes[2].SetNotaPrimerParcial(4);
+            estudiantes[2].SetNotaSegundoParcial(6);
+
+
+           
+
+            //foreach(Estudiante e in estudiantes)
+            //{
+            //    e.Mostrar();
+            //    Console.WriteLine("hola");
+            //}
+
+            foreach (var estudiante in estudiantes)
+            {
+                Console.WriteLine(estudiante.Mostrar());
+            }
+
+        }
+    }
+}
